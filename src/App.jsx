7f9559ae565aceae3b97bc99e9ad77ccde07af9b
Michael Degginger <michael.degginger@phoenix.edu>
2021-06-25 15:03:05 -0600

    added imports for mui

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
-import { Typography, AppBar, Card, CardActions, CardContent, CardMedia, CssBaseline, Grid, Toolbar, Container  } from '@material-ui/core'
+import { Typography, AppBar, Button, Card, CardActions, CardContent, CardMedia, CssBaseline, Grid, Toolbar, Container  } from '@material-ui/core'
 import { PhotoCamera } from '@material-ui/icons';
+import { makeStyles } from '@material-ui/core/styles'
+
+
+const useStyles = makeStyles((theme) => ({
+    container: {
+        backgrounColor: theme.palette.background.paper,
+        padding: theme.spacing(8, 0, 6)
+    }
+}))
 
 
 const App = () => {
+    const classes = useStyles();
+
     return (
        <>
        <CssBaseline />
@@ -16,8 +27,8 @@ const App = () => {
            </Toolbar>
        </AppBar>
        <main>
-           <div>
-               <Container maxWidth='sm' align='center'>
+           <div className={classes.container}>
+               <Container maxWidth='sm' align='center' >
                 <Typography variant='h2'  color='textPrimary'gutterBottom>
                     Photo Album
                 </Typography>
@@ -25,6 +36,21 @@ const App = () => {
                     Lorem ipsum dolor sit amet consectetur, adipisicing elit. Soluta deleniti amet animi, tempore fugit repellendus unde perferendis aut. Veniam libero porro animi, iure nobis pariatur corrupti modi. Suscipit, autem dignissimos!
                     Veniam nisi ut doloremque debitis assumenda minus, optio corrupti totam tenetur accusantium quaerat voluptas modi unde, beatae aliquam aliquid tempora voluptate quis numquam soluta harum cupiditate odio illum sapiente. Culpa?
                 </Typography>
+                <div>
+                    <Grid container spacing={2} justify='center'>
+                        <Grid item>
+                            <Button variant='contained' color='primary'>
+                                See my photos
+                            </Button>
+                        </Grid>
+                        <Grid item>
+                            <Button variant='outlined' color='primary'>
+                                Secondary Action
+                            </Button>
+                        </Grid>
+
+                    </Grid>
+                </div>
                </Container>
            </div>
        </main>

c3b7ae70a7c1e4196dcd463d39d3d373f147676a
Michael Degginger <michael.degginger@phoenix.edu>
2021-06-25 17:34:48 -0600

    basic page done

--- src/App.jsx
+++ src/App.jsx
@@ -1,59 +1,114 @@
-import useStyles from './styles';
-import React from 'react'
-import { Typography, AppBar, Button, Card, CardActions, CardContent, CardMedia, CssBaseline, Grid, Toolbar, Container  } from '@material-ui/core'
-import { PhotoCamera } from '@material-ui/icons';
+import useStyles from "./styles";
+import React from "react";
+import {
+  Typography,
+  AppBar,
+  Button,
+  Card,
+  CardActions,
+  CardContent,
+  CardMedia,
+  CssBaseline,
+  Grid,
+  Toolbar,
+  Container,
+} from "@material-ui/core";
+import { PhotoCamera } from "@material-ui/icons";
 
-
-
-
+const cards = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
 const App = () => {
-    const classes = useStyles();
+  const classes = useStyles();
 
-    return (
-       <>
-       <CssBaseline />
-       <AppBar position='relative'>
-           <Toolbar>
-               <PhotoCamera className={classes.icon} />
-               <Typography variant ='h6'>
-                   Photo Album
-               </Typography>
-           </Toolbar>
-       </AppBar>
-       <main>
-           <div className={classes.container} >
-               <Container maxWidth='sm' align='center'>
-                <Typography variant='h2'  color='textPrimary' gutterBottom>
-                    Photo Album
-                </Typography>
-                <Typography variant = 'h5'  color='textSecondary' paragraph>
-                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Soluta deleniti amet animi, tempore fugit repellendus unde perferendis aut. Veniam libero porro animi, iure nobis pariatur corrupti modi. Suscipit, autem dignissimos!
-                    Veniam nisi ut doloremque debitis assumenda minus, optio corrupti totam tenetur accusantium quaerat voluptas modi unde, beatae aliquam aliquid tempora voluptate quis numquam soluta harum cupiditate odio illum sapiente. Culpa?
-                </Typography>
-                <div className= {classes.button} >
-                    <Grid container spacing={2} justify='center'>
-                        <Grid item>
-                            <Button variant='contained' color='primary'>
-                                See my photos
-                            </Button>
-                        </Grid>
-                        <Grid item>
-                            <Button variant='outlined' color='primary'>
-                                Secondary Action
-                            </Button>
-                        </Grid>
-
-                    </Grid>
-                </div>
-               </Container>
-           </div>
-           <Container>
-               
-           </Container>
-       </main>
-       </>
-    )
-}
+  return (
+    <>
+      <CssBaseline />
+      <AppBar position="relative">
+        <Toolbar>
+          <PhotoCamera className={classes.icon} />
+          <Typography variant="h6">Photo Album</Typography>
+        </Toolbar>
+      </AppBar>
+      <main>
+        <div className={classes.container}>
+          <Container maxWidth="sm">
+            <Typography
+              variant="h2"
+              color="textPrimary"
+              align="center"
+              gutterBottom
+            >
+              Photo Album
+            </Typography>
+            <Typography
+              variant="h5"
+              color="textSecondary"
+              align="center"
+              paragraph
+            >
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Soluta
+              deleniti amet animi, tempore fugit repellendus unde perferendis
+              aut. 
+            </Typography>
+            <div className={classes.button}>
+              <Grid container spacing={2} justify="center">
+                <Grid item>
+                  <Button variant="contained" color="primary">
+                    See my photos
+                  </Button>
+                </Grid>
+                <Grid item>
+                  <Button variant="outlined" color="primary">
+                    Secondary Action
+                  </Button>
+                </Grid>
+              </Grid>
+            </div>
+          </Container>
+        </div>
+        <Container className={classes.cardGrid} maxWidth="md">
+          <Grid container spacing={4}>
+            {cards.map((card) => (
+              <Grid item key={card} xs={12} sm={6} md={4}>
+                <Card className={classes.card}>
+                  <CardMedia
+                    className={classes.cardMedia}
+                    image="https://source.unsplash.com/random"
+                    title="Image title"
+                  />
+                  <CardContent className={classes.cardContent}>
+                    <Typography gutterBottom variant="h5">
+                      Heading
+                    </Typography>
+                    <Typography>
+                      Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                      
+                    </Typography>
+                  </CardContent>
+                  <CardActions>
+                    <Button size="small" color="primary">
+                      View
+                    </Button>
+                    <Button size="small" color="primary">
+                      Edit
+                    </Button>
+                  </CardActions>
+                </Card>
+              </Grid>
+            ))}
+          </Grid>
+        </Container>
+      </main>
+      <footer className={classes.footer}>
+            <Typography variant='h6' align='center' gutterBottom>
+                Footer
+            </Typography>
+            <Typography variant='subtitle1' align='center' color='textSecondary' >
+               something here to give the footer purpose
+            </Typography>
+      </footer>
+    </>
+  );
+};
 
 export default App;
--- src/styles.js
+++ src/styles.js
@@ -10,6 +10,24 @@ const useStyles = makeStyles((theme) => ({
   },
   button: {
     marginTop: "40px",
+  },
+  cardGrid: {
+    padding: '20px 0',
+  },
+  card: {
+    height: '100%',
+    display: 'flex',
+    flexDirection: 'column'
+  },
+  cardMedia: {
+    paddingTop: '56.25%'
+  },
+  cardContent: {
+    flexGrow: 1,
+  },
+  footer: {
+    backgroundColor: theme.palette.background.paper,
+    padding: "50px 0"
   }
 }));
 
